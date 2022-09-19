c6a075a09ed871a73163cdcbf3492886b09b5b62
malfaro21 <malfaro21@students.desu.edu>
2022-09-18 22:55:28 -0400

    end

--- cypher.py
+++ cypher.py
@@ -3,17 +3,36 @@
 # Recitation 1 - Encryption with a password
 # *******************************************************
 
+import math
 import string
+alphabet = string.printable
+ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 alphabet = string.printable
 ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
-
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+
+    for index, ch in enumerate(message):
+        pass_ch = password[index % len(password)]
+        key = ordinal_value[pass_ch]
+        ord_of_character = ordinal_value[ch]
+        shifted_ord_of_characters = (ord_of_character + key) % len(alphabet)
+        encrypted_characters = alphabet[shifted_ord_of_characters]
+        encrypted_message += encrypted_characters
+    return encrypted_message
 
 
 def decrypt(message, password):
-    decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    encrypted_message = ''
+
+    for index, ch in enumerate(message):
+        pass_ch = password[index%len(password)]
+        key = ordinal_value[pass_ch]
+        ord_of_character = ordinal_value[ch]
+        shifted_ord_of_characters = (ord_of_character - key) % len(alphabet)
+        encrypted_characters = alphabet[shifted_ord_of_characters]
+        encrypted_message += encrypted_characters
+
+    return encrypted_message
